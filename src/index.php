5b39e3b8dbd334544b5b7a93bfea6ae1ff6ef045
Valentín <htmgarcia@gmail.com>
2016-04-14 15:23:28 -0500

    HTML5 support

--- src/index.php
+++ src/index.php
@@ -118,7 +118,7 @@ if( file_exists('templates/'.$this->template.'/css/custom.css')){
 JHtml::_('bootstrap.loadCss', false, $this->direction);
 ?>
 
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $this->language; ?>" lang="<?php echo $this->language; ?>" >
 <head>
 	<jdoc:include type="head" />
